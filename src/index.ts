751786e51506d17a7724a98c911ae6d8360be643
Dean Agan <agandfr@gmail.com>
2022-09-22 19:55:55 +1000

    Add chalk for better logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express from "express";
+import chalk from "chalk";
 
 const app = express();
 const PORT = 8081;
 app.get("/hello", (_req, res) => res.send("Hello basic setup!"));
 app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`[server]: Server is running at http://localhost:${chalk.green(PORT)}`);
 });
