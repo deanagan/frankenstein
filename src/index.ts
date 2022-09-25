cd0de8a7dd3b55020462f404ade94cf0ac01ac31
Dean Agan <agandfr@gmail.com>
2022-09-25 17:33:24 +1000

    Error loggers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import chalk from "chalk";
 import Debug from "debug";
 import * as pokemonController from "./controllers/pokemon.controller";
+import errorHelpers from "./helpers/error.helpers";
 
 const app = express();
 const debug = Debug("app");
@@ -20,6 +21,10 @@ router.put("/pokemon/:id", pokemonController.updatePokemon);
 router.delete("/pokemon/:id", pokemonController.deletePokemon);
 
 app.use("/api/", router);
+
+app.use(errorHelpers.logErrors);
+app.use(errorHelpers.clientErrorHandler);
+app.use(errorHelpers.errorHandler);
 
 app.listen(PORT, () => {
   debug(`[server]: Server is running at http://localhost:${chalk.green(PORT)}`);
--- src/types.ts/common.ts
+++ src/types.ts/common.ts
@@ -1,4 +1,24 @@
 type RejectCallback = (arg0: NodeJS.ErrnoException) => void;
 type ResolveCallback = (arg0: unknown) => void;
+type ErrorMessage = {
+  status: number;
+  statusText: string;
+  message: string;
+  error: {
+    errno: number;
+    call: string;
+    code: string;
+    message: string;
+  };
+};
 
-export { RejectCallback, ResolveCallback };
+type ErrorDetail = {
+  code: number;
+  errno: number;
+  path: string;
+  syscall: string;
+  message: string;
+  stack: string;
+};
+
+export { RejectCallback, ResolveCallback, ErrorMessage, ErrorDetail };
