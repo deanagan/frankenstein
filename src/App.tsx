8c66628713dd63707f1eb609ec38ad3ac48e5f52
Dean Agan <agandfr@gmail.com>
2023-07-28 21:49:20 +1000

    Some styling adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="dark:bg-dark">
       <div className="container mx-auto py-8">
         <h1 className="text-2xl font-bold mb-4">Shootin Cards Inventory</h1>
-        <div className="grid grid-cols-6 gap-4">
+        <div className="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
