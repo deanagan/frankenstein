79c63323e99247fbe87cab96284395c95c8718ac
Dean Agan <agandfr@gmail.com>
2024-05-01 13:05:33 +1000

    Adding in test codes to embed links on a video

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,29 +3,98 @@ import React, { useState } from "react";
 import ProductCard from "./components/ProductCard";
 import products from "./products";
 
-import { setName } from "./appActions"
+import { setName } from "./appActions";
 import { useDispatch } from "react-redux";
 import { useLocalStorageState } from "./hooks/localStorage";
 
 type InputChangeEvent = React.ChangeEvent<HTMLInputElement>;
+const video_address =
+  "https://github.com/deanagan/seed-collection/raw/main/frontend/public/videos/sample_video.mp4";
 
+function VideoWithMultipleLinks() {
+  // TODO: Move out to CSS files. Just for testing now.
+  return (
+    <div style={{ position: "relative", display: "flex" }}>
+      <a
+        href="https://www.theseedcollection.com.au/Garden-&-Flower-Scissors"
+        style={{
+          position: "absolute",
+          top: 510,
+          left: 120,
+          zIndex: 10,
+          display: "inline-block",
+          width: "20px",
+          height: "20px",
+          backgroundColor: "red",
+          borderRadius: "50%",
+        }}
+        title="Garden & Flower Scissors (RYSET)"
+      ></a>
+      <a
+        href="https://www.theseedcollection.com.au/microgreen-seeds-kale-red-russian-p"
+        style={{
+          position: "absolute",
+          top: 710,
+          left: 1210,
+          zIndex: 10,
+          display: "inline-block",
+          width: "20px",
+          height: "20px",
+          backgroundColor: "green",
+          borderRadius: "50%",
+        }}
+        title="Microgreen Seeds- Kale Red Russian"
+      ></a>
+
+      <video
+        width={900}
+        height="50%"
+        controls
+        style={{
+          display: "flex",
+          alignItems: "stretch",
+          justifyContent: "center",
+          width: "100%",
+        }}
+        autoPlay
+        muted
+        loop
+      >
+        <source src={video_address} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
 
 function App() {
-  const [value, setValue] = useState<string>('');
-  const dispatch = useDispatch();
+  // const [value, setValue] = useState<string>("");
+  // const dispatch = useDispatch();
 
-  const [localStorageName, setLocalStorageName] = useLocalStorageState("name", "No Name");
+  // const [localStorageName, setLocalStorageName] = useLocalStorageState(
+  //   "name",
+  //   "No Name"
+  // );
 
-  // Function to handle input change
-  const handleChange = (event: InputChangeEvent) => {
-    setValue(event.target.value);
-    setLocalStorageName(event.target.value);
-  }
+  // // Function to handle input change
+  // const handleChange = (event: InputChangeEvent) => {
+  //   setValue(event.target.value);
+  //   setLocalStorageName(event.target.value);
+  // };
 
   return (
-    <div >
+    <div>
       <div className="container mx-auto py-8">
-        <h1 className="text-2xl font-bold mb-4">Seed Collection Inventory</h1>
+        <h1 className="text-2xl font-bold mb-4">
+          Welcome to the Seed Collection!
+        </h1>
+        <h2>
+          At the Seed Collection, we bring you a unique and immersive shopping
+          experience like never before. Explore our curated collection of
+          products showcased in captivating videos, where every frame is an
+          opportunity to discover something extraordinary.
+        </h2>
+        <VideoWithMultipleLinks />
         <div className="sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
@@ -33,14 +102,17 @@ function App() {
         </div>
       </div>
 
-      <div>
+      {/* <div>
         <label htmlFor="name-text">Name: {localStorageName}</label>
-        <input id="name-text" type="text" onChange={handleChange}/>
-     
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() =>  dispatch(setName(value))}>
+        <input id="name-text" type="text" onChange={handleChange} />
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => dispatch(setName(value))}
+        >
           Set Name
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
