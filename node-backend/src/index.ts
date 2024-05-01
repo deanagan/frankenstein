7bd3287e800942bfbae9256a494bf16efc49cd5a
Dean Agan <agandfr@gmail.com>
2024-05-01 13:38:02 +1000

    Add some test codes for the node backend api

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -1,2 +1,12 @@
-// src/index.ts
-console.log("Hello from TypeScript!");
+import express from "express";
+
+const app = express();
+const port = 3002;
+
+app.get("/", (req, res) => {
+  res.send("Test get endpoint");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
